dd3b51aa0f6321149866be672cbb697c6b8bb66a
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-12-11 19:43:33 +0100

    Setupped cors session, router, cookies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import express, { Request, Response } from "express";
 import { Server } from "socket.io";
+import routers from "./routers";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import mongoDbStore from "connect-mongo";
+import { config } from "dotenv"
+import cors from "cors";
+import { mongo } from "mongoose";
+require("./database");
 const app = express();
+config();
 
-app.get("/", (req: Request, res: Response) => {
-    res.sendStatus(200);
-})
+app.use(express.json);
+app.use(express.urlencoded({
+    extended: false,
+}))
+
+app.use(cors({
+    origin: ["https://localhost:3000/"],
+    credentials: true,
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SEECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 60 * 24 * 30
+    },
+    store: mongoDbStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
+}))
+
+app.use(cookieParser())
+
+app.use("/api", routers);
 
 app.listen(3001, () => console.log("App listening on port http://localhost:3001/"))
