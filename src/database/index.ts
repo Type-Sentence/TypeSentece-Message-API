4d75ab8c0420185a26f3918d79ef044aab94a9ef
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-12-11 19:44:25 +0100

    Connected mongodb to the api

---
+++ src/database/index.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_URI!)
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.error(err));
