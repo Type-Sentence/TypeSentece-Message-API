1a9cb53bb0eab455dc6930ad6dbf8590a6dba439
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-01-15 02:28:48 +0100

    Created a snowflakes based id sysytem

---
+++ src/helpers/auth/generateUserId.ts
@@ -0,0 +1,30 @@
+import { isDate } from "util/types";
+
+export const generateSnowFlakes = () => {
+    const epoch = 1420070400000 //Discord Epoch Thursday 1 January 2015 00:00:00
+    const snowflakes = Number((BigInt(Date.now() - epoch) << 22n) + 1n);
+    return snowflakes;
+}
+
+export const snowFlakesToDate = (snowflakes: number) => {
+    const epoch = 1420070400000 //Discord Epoch Thursday 1 January 2015 00:00:00
+    const milliseconds = BigInt(snowflakes) >> 22n;
+    const date = new Date(Number(milliseconds) + epoch)
+    return date;
+}
+
+export const dateToSnowFlakes = (date: number | Date) => {
+    const epoch = 1420070400000 //Discord Epoch Thursday 1 January 2015 00:00:00
+
+    if (isDate(date)) {
+        console.log("Is Date")
+        const milliseconds = BigInt(date.getTime() - epoch);
+        const snowflakes = Number(milliseconds << 22n);
+        return snowflakes;
+    } else if (typeof date === "number") {
+        console.log("Is Nubmer")
+        const milliseconds = BigInt(date - epoch);
+        const snowflakes = Number(milliseconds << 22n);
+        return snowflakes;
+    }
+}
