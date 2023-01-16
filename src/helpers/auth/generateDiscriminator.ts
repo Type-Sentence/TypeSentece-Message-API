292b307dc09dbf712924ba30bf9d427e2c37bf21
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-01-16 18:37:36 +0100

    Created a function for generate the discriminator

---
+++ src/helpers/auth/generateDiscriminator.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import Users from "../../database/schemas/usersSchema";
+import { User } from "../../interfaces/userInterafaces";
+
+export const generateDiscriminator = async (req: Request, res: Response, username: string) => {
+    try {
+        const users = await Users.find<User>({ username })  // Return an array of users with that specified username, or return an empty array
+
+        if (users.length <= 0) return "#0001";
+
+        if (users) {
+            console.log(users);
+            const lastCreatedUsers: User = users[users.length - 1];
+
+            const lastDiscriminator = lastCreatedUsers.discriminator;
+
+            let getDiscriminatorNumber = parseInt(lastDiscriminator.substring(1), 10);
+
+            if (getDiscriminatorNumber >= 9999) {
+                return res.status(403).send({ msg: "There are to many users with this username" })
+            }
+            getDiscriminatorNumber++
+            const discriminator = getDiscriminatorNumber.toString().padStart(4, "0");
+            const formattedDiscriminator = `#${discriminator}`
+
+            return formattedDiscriminator;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
